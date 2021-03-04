83906f3a10c30fab078678a6ee7fde4455f0a4f0
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2021-03-04 14:32:11 +0100

    Shoppingcard

---
+++ products.js
@@ -0,0 +1,39 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  },
+  124: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  },
+  125: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  },
+  126: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  },
+  127: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  },
+  128: {
+    name : "Plant ",
+    desc : "beschrijving",
+    img : "logo.png",
+    price : 111
+  }
+};
