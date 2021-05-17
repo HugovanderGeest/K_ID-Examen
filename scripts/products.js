14c3c1cf2d7bde2563b80f00800e54486bad2ccf
Hugo van der Geest <Hugo.vdg@outlook.com>
2021-05-17 21:07:08 +0200

    Responsive fixes

--- scripts/products.js
+++ scripts/products.js
@@ -1,45 +1,39 @@
 // DUMMY PRODUCTS (PRODUCT ID : DATA)
 var products = {
   123: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "cactus.jpg",
-    price : 111
+    name : "Bamboe ",
+    desc : "Bamboe is een inheemse plant in Azië, China, Japan, Midden- en Zuid -Amerika, Afrika en in Australië",
+    img : "bamboe.jpg",
+    price : 39
   },
   124: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
+    name : "Orchidee ",
+    desc : "Orchidaceae is wellicht de grootste plantenfamilie. In veel landen groeien orchideeën",
+    img : "Orchidee.jpg",
+    price : 19.50
   },
   125: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
+    name : "Rozen ",
+    desc : "Rozen zijn één van de meeste geliefde en bezongen planten en behoren tot de bladverliezende heesters. ",
+    img : "roses.jpg",
+    price : 12.50
   },
   126: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
+    name : "Cactus ",
+    desc : "Succulenten (ook wel vetplanten genoemd) komen in grote delen van de wereld voor",
+    img : "Cactus.jpg",
+    price : 10
   },
   127: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
+    name : "Bonsai ",
+    desc : "Mensen hebben waarschijnlijk al duizenden jaren lang planten in potten gezet om hun woonomgeving te decoreren",
+    img : "Bonsai.jpg",
+    price : 22.00
   },
   128: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
-  },
-  129: {
-    name : "Plant ",
-    desc : "beschrijving",
-    img : "logo.png",
-    price : 111
-  },
+    name : "Varens ",
+    desc : "Varens zijn bij ieder van ons welbekend. Zo niet, maak dan eens een boswandeling dan ziet u vaak de overvloed van varens op de bodem groeien",
+    img : "ferns.jpg",
+    price : 6
+  }
 };
